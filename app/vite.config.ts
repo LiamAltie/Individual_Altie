2fd57cf93b15aa3ccd96f8e7156b96aff49da31f
Liam <62319582+LiamAltie@users.noreply.github.com>
2024-09-13 10:56:12 +0000

    optimize to Netlify

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const repositoryName = "Individual_Altie";
-
 export default defineConfig({
-  base: `/${repositoryName}/`,
+  base: `/`,
   plugins: [react()],
 });
